e7b200d79f8963de19483f9b37b7988d494e5cc1
Fabio Marques <fabio.cmarques@hotmail.com>
2016-12-19 22:14:59 -0200

    Full design of rotation, but not tested.

--- rotation.vhd
+++ rotation.vhd
@@ -412,6 +412,7 @@ begin
 		block2count <= 0;
 		rotating <= '0';
 	
+-- Block_type 3 it's a square, which does not require any rotation effort.	
 	
 
 elsif(rising_edge(clk) and block_type = 4 and roda = '1') then
@@ -519,6 +520,148 @@ elsif(rising_edge(clk) and block_type = 4 and block4count = 4) then
 elsif(rising_edge(clk) and block_type = 4 and block4count = 5) then
 		makeRotation <= '0';
 		block4count <= 0;
+		rotating <= '0';
+		
+elsif(rising_edge(clk) and block_type = 5 and roda = '1') then
+		rotating <= '1';
+		block0count <= 0;
+		block1count <= 0;
+		block2count <= 0;
+		block3count <= 0;
+		block4count <= 0;
+		block5count <= 1;
+		block6count <= 0;
+		naoRoda <= '0';
+		
+		if(j1 = j3 and i1 /= i2 and i2 = i3 and j3 /= j4) then
+			i <= i1; 
+			j <= j1; 
+		elsif(j1 = j2 and i2 = i3 and j1 = j4) then
+			i <= i1 + 1;
+			j <= j1 - 1;
+			if(j1 = 1) then
+				j <= 1;
+			end if;
+		elsif(j2 = j4 and i1 /= i4 and i1 = i3) then
+			i <= i1 - 1;
+			j <= j1 + 1;
+		else
+			i <= i1; 
+			j <= j1; 
+			if(j1 = 9) then
+				j <= 8;
+			end if;
+		end if;
+		
+		block5count <= 1;
+		
+	elsif(rising_edge(clk) and block_type = 5 and block5count = 1) then
+		if(isFilled = '1') then
+			naoRoda <= '1';
+		else
+			i1out <= i;
+			j1out <= j;
+		end if;
+		
+		if(j1 = j3 and i1 /= i2 and i2 = i3 and j3 /= j4) then
+			i <= i2; 
+			j <= j2 + 1;
+		elsif(j1 = j2 and i2 = i3 and j1 = j4) then
+			i <= i2; 
+			j <= j2; 
+			if(j1 = 1) then
+				j <= 2;
+			end if;
+		elsif(j2 = j4 and i1 /= i4 and i1 = i3) then
+			i <= i2; 
+			j <= j2 - 1;
+		else
+			i <= i2; 
+			j <= j2; 
+			if(j1 = 9) then
+				j <= 7;
+			end if;
+		end if;
+		
+		block5count <= 2;
+		
+	elsif(rising_edge(clk) and block_type = 5 and block5count = 2) then
+		if(isFilled = '1') then
+			naoRoda <= '1';
+		else
+			i2out <= i;
+			j2out <= j;
+		end if;
+		
+		if(j1 = j3 and i1 /= i2 and i2 = i3 and j3 /= j4) then
+			i <= i3;
+			j <= j3 + 1; 
+		elsif(j1 = j2 and i2 = i3 and j1 = j4) then
+			i <= i3;
+			j <= j3;
+			if(j1 = 1) then
+				j <= 3;
+			end if;
+		elsif(j2 = j4 and i1 /= i4 and i1 = i3) then
+			i <= i3; 
+			j <= j3 - 1;
+		else
+			i <= i3; 
+			j <= j3; 
+			if(j1 = 9) then
+				j <= 8;
+			end if;
+		end if;
+		
+		block5count <= 3;
+		
+	elsif(rising_edge(clk) and block_type = 5 and block5count = 3) then
+		if(isFilled = '1') then
+			naoRoda <= '1';
+		else
+			i3out <= i;
+			j3out <= j;
+		end if;
+		
+		if(j1 = j3 and i1 /= i2 and i2 = i3 and j3 /= j4) then
+			i <= i4 + 1;
+			j <= j4 - 1;
+		elsif(j1 = j2 and i2 = i3 and j1 = j4) then
+			i <= i4; 
+			j <= j4; 
+			if(j1 = 1) then
+				j <= 2;
+			end if;
+		elsif(j2 = j4 and i1 /= i4 and i1 = i3) then
+			i <= i4; 
+			j <= j4; 
+		else
+			i <= i4 - 1;
+			j <= j4 + 1;
+			if(j1 = 9) then
+				j <= 9;
+			end if;
+		end if;
+		
+		block5count <= 4;
+		
+	elsif(rising_edge(clk) and block_type = 5 and block5count = 4) then
+		if(isFilled = '1') then
+			naoRoda <= '1';
+		else
+			i4out <= i;
+			j4out <= j;
+		end if;
+		
+		if(naoRoda = '0') then
+			makeRotation <= '1';
+		end if;
+		
+		block5count <= 5;
+		
+	elsif(rising_edge(clk) and block_type = 5 and block5count = 5) then
+		makeRotation <= '0';
+		block5count <= 0;
 		rotating <= '0';
 		
 elsif(rising_edge(clk) and block_type = 6 and roda = '1') then
